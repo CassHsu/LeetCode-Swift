4477b9148266cb53058f8d56fed1ec0beab178d8
CassHsu <cass.hsu@gmail.com>
2022-12-07 09:40:31 +0800

    Create 0876-01.swift

---
+++ 0876/0876-01.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var fast = head;
+        var slow = head;
+
+        while (fast != nil && fast?.next != nil) {
+            fast = fast?.next?.next
+            slow = slow?.next
+        }
+
+        return slow;
+    }
+}
