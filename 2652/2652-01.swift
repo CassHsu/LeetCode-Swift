0a94c3b02c0d27bd7fc7eff8748e6f9aa6aa6de1
CassHsu <cass.hsu@gmail.com>
2023-05-11 23:31:54 +0800

    Create 2652-01.swift

---
+++ 2652/2652-01.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func sumOfMultiples(_ n: Int) -> Int {
+        var ans = 0
+
+        for i in 1...n {
+            if i % 3 == 0 || i % 5 == 0 || i % 7 == 0 {
+                ans += i
+            }
+        }
+
+        return ans
+    }
+}
